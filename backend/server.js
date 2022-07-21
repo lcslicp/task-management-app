5226e7e622418aa0bcf9692802ae688c7e7621bc
Leslie Pino <contactlespino@gmail.com>
2022-07-21 13:02:27 +0800

    Express endpoint

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cookieParser());
 
-
+app.get('/', (req, res) => { res.send('Hello from Express!')});
 app.use('/', userRoutes);
 app.use('/', verifyJWT);
 app.use('/', taskRoutes);
