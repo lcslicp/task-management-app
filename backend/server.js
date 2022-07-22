4fd3a935414bc8a19c8933ecf20a729345a11072
Leslie Pino <contactlespino@gmail.com>
2022-07-22 10:28:29 +0800

    configured headers

--- backend/server.js
+++ backend/server.js
@@ -22,12 +22,14 @@ app.use(cors({
     origin: ['https://doowit.netlify.app/'],
 }));
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "https://doowit.netlify.app/");
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-//   });
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', 'https://doowit.netlify.app/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cookieParser());
