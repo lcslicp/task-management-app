0d4f607b5867f61c09d91e02f20d2a4ca424e361
Leslie Pino <contactlespino@gmail.com>
2023-03-28 09:36:54 +0800

    Edited path method.

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -6,7 +6,7 @@ const __dirname = fileURLToPath(import.meta.url);
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, '../public'))
+      cb(null, path.resolve(__dirname, '../public'))
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
