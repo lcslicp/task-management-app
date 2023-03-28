496743608fa28a1bb09ba5b45674087379c9c6e3
Leslie Pino <contactlespino@gmail.com>
2023-03-28 09:23:12 +0800

    Fixed public directory.

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,5 +1,8 @@
-import multer from 'multer';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import multer from 'multer';
+
+const __dirname = fileURLToPath(import.meta.url);
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
