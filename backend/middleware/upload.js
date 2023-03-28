1cb55a17f8cb1be2b488645bd0e6dfc7e8d1ee4a
Leslie Pino <contactlespino@gmail.com>
2023-03-28 09:22:53 +0800

    Fixed public directory.

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,8 +1,9 @@
 import multer from 'multer';
+import path from 'path';
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'public')
+      cb(null, path.join(__dirname, '../public'))
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
