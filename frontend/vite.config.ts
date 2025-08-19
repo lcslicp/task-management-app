8c3495f5c2d4c2d695d67154c45a720191950085
Leslie P <contactlespino@gmail.com>
2025-08-19 20:15:57 +0800

    reconfigure for deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,18 @@
-// import { defineConfig } from 'vite';
-// import svgr from 'vite-plugin-svgr';
-// import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     svgr({
-//       include: '**/*.svg?react',
-//     }),
-//   ],
-// });
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+},
+build: {
+  rollupOptions: {
+    external: [
+      /^node:.*/,
+      '/node_modules/jwt-decode'
+    ]
+  }
+}
+})
