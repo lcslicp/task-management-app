b85cc35d575a94438aecd26dc15767e917ddf8d1
Leslie P <contactlespino@gmail.com>
2025-08-20 11:44:53 +0800

    Fix fetch task functionality

--- frontend/src/features/tasks/taskThunks.ts
+++ frontend/src/features/tasks/taskThunks.ts
@@ -1,15 +1,14 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "../../api/axios";
 
-const token = JSON.parse(localStorage.getItem("token") || "{}");
-const config = {
-  headers: { Authorization: `Bearer ${token}` },
-};
-
 export const fetchSingleTaskData = createAsyncThunk(
   "tasks/fetchSingleTaskData",
   async (id: string, thunkAPI) => {
     try {
+      const token = JSON.parse(localStorage.getItem("token") || "{}");
+      const config = {
+        headers: { Authorization: `Bearer ${token}` },
+      };
       const response = await axios.get(`/task/${id}`, config);
       return response.data;
     } catch (error: any) {
