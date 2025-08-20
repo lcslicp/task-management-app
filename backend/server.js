b7fa35453557ba2306f161c950323936a10cc79c
Leslie P <contactlespino@gmail.com>
2025-08-20 09:49:50 +0800

    Reconfigure for deployment server

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ import cors from 'cors';
 import logger from 'morgan';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = fileURLToPath(import.meta.url);
 import errorHandler from './middleware/errorHandler.js';
 
 import taskRoutes from './routes/taskRoutes.js';
@@ -11,19 +14,25 @@ import userRoutes from './routes/userRoutes.js';
 import verifyJWT from './middleware/veriryJWT.js';
 
 dotenv.config();
-
 const app = express();
 
+const PORT = process.env.PORT || 5002;
 app.use(logger('dev'));
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:3000']
+    origin: ['https://doowit.lcslicp.xyz']
 }));
+app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'https://doowit.lcslicp.xyz');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+  });
 app.use(cookieParser());
 
-app.use(express.static('uploads'));
+app.get('/', (req, res) => { res.send('Hello from Express!')});
 app.use('/', userRoutes);
 app.use('/', verifyJWT);
 app.use('/', taskRoutes);
@@ -40,6 +49,12 @@ mongoose.connect(process.env.DB_HOST, {
     process.exit(1)
 });
 
-const PORT = process.env.PORT || 5002;
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/dist' ));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+    })
+}
 
 app.listen(PORT, console.log(`Server running in Port ${PORT}`));
